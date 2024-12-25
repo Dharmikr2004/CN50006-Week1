6e0c5debea66bb321d89c1fb55fd68413adc4df8
Dharmikr2004 <dharmikuk22@gmail.com>
2024-12-25 03:15:39 +0000

    Add files via upload

---
+++ Week 1/index.js
@@ -0,0 +1,12 @@
+const num1=8;
+const num2=2;
+
+const sum=num1+num2;
+const sub=num1-num2;
+const mult=num1*num2;
+const div=num1/num2;
+
+console.log("The sum is: "+sum);
+console.log("The substration is: "+sub);
+console.log("The multiplication is: "+mult);
+console.log("The division is: "+div);
